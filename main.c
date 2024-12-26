e2c464601263a4b92f0d5442bbca625e45e94a10
ZuZijie <14937125+zoinve@user.noreply.gitee.com>
2024-12-26 19:15:48 +0800

    compile

--- main.c
+++ main.c
@@ -1,32 +1,23 @@
 #include "ramfs.h"
 #include "shell.h"
-#include <assert.h>
 #include <string.h>
+#include <stdlib.h>
+#include <assert.h>
 
-extern node *root;
-const char *content = "export PATH=$PATH:/usr/bin/\n";
+const char *content = "export PATH=/usr/bin/\n";
 const char *ct = "export PATH=/home:$PATH";
 int main() {
-  init_ramfs();
+    init_ramfs();
 
-  smkdir("/home");
-  smkdir("/home/ubuntu");
-  smkdir("/usr");
-  smkdir("/usr/bin");
-  stouch("/home/ubuntu/.bashrc");
-  rwrite(ropen("/home/ubuntu/.bashrc", O_WRONLY), content, strlen(content));
-  rwrite(ropen("/home/ubuntu/.bashrc", O_WRONLY | O_APPEND), ct, strlen(ct));
-  scat("/home/ubuntu/.bashrc");
+    assert(rmkdir("/home") == 0);
+    assert(rmkdir("//home") == -1);
+    assert(rmkdir("/test/1") == -1);
+    assert(rmkdir("/home/ubuntu") == 0);
+    assert(rmkdir("/usr") == 0);
+    assert(rmkdir("/usr/bin") == 0);
+    assert(rwrite(ropen("/home///ubuntu//.bashrc", O_CREAT | O_WRONLY), content, strlen(content)) == strlen(content));
 
-  init_shell();
-  swhich("ls");
-  stouch("/usr/bin/ls");
-  swhich("ls");
-  stouch("/home/ls");
-  swhich("ls");
-  secho("hello world");
-  secho("The Environment Variable PATH is:\\$PATH");
-  close_ramfs();
-  close_shell();
-  assert(root==NULL);
+    int fd = ropen("/home/ubuntu/.bashrc", O_RDONLY);
+    char buf[105] = {0};
+    return 0;
 }
