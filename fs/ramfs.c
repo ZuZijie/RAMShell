b47383d9a50db8fd8592f7d68996b285c1ef197e
ZuZijie <14937125+zoinve@user.noreply.gitee.com>
2024-12-26 19:56:38 +0800

    compile

--- fs/ramfs.c
+++ fs/ramfs.c
@@ -181,6 +181,7 @@ int rmkdir(const char *pathname) {//make new directory
         for(int i=lastposi;i<strlen(pathname);i++)
         newDirName[i-lastposi]=pathname[i];
         node *newDir=(node *)malloc(sizeof(node));
+        newDir->name=(char *)malloc(strlen(newDirName)+1);
         strcpy(newDir->name,newDirName);
         pt_fatherNode->dirents=malloc(sizeof(node*));
         pt_fatherNode->dirents[pt_fatherNode->nrde]=newDir;
