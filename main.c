5555dada2eb7730b0a7b4ede9c074bc4bc055df1
ZuZijie <14937125+zoinve@user.noreply.gitee.com>
2024-12-28 15:49:14 +0800

    compile

--- main.c
+++ main.c
@@ -3,22 +3,19 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
-#include  "stdio.h"
 
 const char *content = "export PATH=/usr/bin/\n";
 const char *ct = "export PATH=/home:$PATH";
 int main() {
     init_ramfs();
 
-    assert(rmkdir("/home") == 1 );
+    assert(rmkdir("/home") == 0);
     assert(rmkdir("//home") == -1);
-    // assert(rmkdir("/home/home/1") == -1);
+    // assert(rmkdir("/test/1") == -1);
     // assert(rmkdir("/home/ubuntu") == 0);
     // assert(rmkdir("/usr") == 0);
     // assert(rmkdir("/usr/bin") == 0);
     // assert(rwrite(ropen("/home///ubuntu//.bashrc", O_CREAT | O_WRONLY), content, strlen(content)) == strlen(content));
 
-    // int fd = ropen("/home/ubuntu/.bashrc", O_RDONLY);
-    char buf[105] = {0};
-    return 0;
+   return  0;
 }
