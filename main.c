8d5509afd5a9d4ac02594bfa133fe8e15329a2dc
ZuZijie <14937125+zoinve@user.noreply.gitee.com>
2024-12-28 15:58:55 +0800

    compile

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main() {
     assert(rmkdir("//home") == -1);
     assert(rmkdir("/test/1") == -1);
     assert(rmkdir("/home/ubuntu") == 0);
-    // assert(rmkdir("/usr") == 0);
+    assert(rmkdir("/usr") == 0);
     // assert(rmkdir("/usr/bin") == 0);
     // assert(rwrite(ropen("/home///ubuntu//.bashrc", O_CREAT | O_WRONLY), content, strlen(content)) == strlen(content));
 
