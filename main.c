c6cc06e1669b2861ecdb3dd56b0064aff587b81b
ZuZijie <14937125+zoinve@user.noreply.gitee.com>
2024-12-30 14:31:28 +0800

    compile

--- main.c
+++ main.c
@@ -55,10 +55,10 @@ int main() {
       assert(memcmp(buf, "\0\0\0\0\0\0\0\0\0\0", 10) == 0);
       assert(rread(fd[1], buf, 10) == 10);
       assert(memcmp(buf, "hellohello", 10) == 0);
-      // assert(rread(fd[2], buf, 10) == 10);
-      // assert(memcmp(buf, "worldworld", 10) == 0);
-      // assert(rread(fd[3], buf, 10) == 10);
-      // assert(memcmp(buf, "\x001\x002\x003\x0fe\x0ff\x001\x002\x003\x0fe\x0ff", 10) == 0);
+      assert(rread(fd[2], buf, 10) == 10);
+      assert(memcmp(buf, "worldworld", 10) == 0);
+      assert(rread(fd[3], buf, 10) == 10);
+      assert(memcmp(buf, "\x001\x002\x003\x0fe\x0ff\x001\x002\x003\x0fe\x0ff", 10) == 0);
     }
     // assert(rread(fd[0], buf, 10) == 0);
     // assert(rread(fd[1], buf, 10) == 0);
