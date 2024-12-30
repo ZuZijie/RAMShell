9f27035fe2ea823e59baace11ce91e77fc3e1472
ZuZijie <14937125+zoinve@user.noreply.gitee.com>
2024-12-30 09:09:27 +0800

    compile

--- main.c
+++ main.c
@@ -29,7 +29,7 @@ int main() {
     assert((fd[3] = ropen("/3", O_CREAT | O_WRONLY)) >= 0);
     assert(rread(fd[0], buf, 1) == -1);
     assert(rread(fd[1], buf, 1) == -1);
-    // assert(rread(fd[2], buf, 1) == -1);
+    assert(rread(fd[2], buf, 1) == -1);
     // assert(rread(fd[3], buf, 1) == -1);
     // for (int i = 0; i < 100; i++) {
     //     assert(rwrite(fd[0], "\0\0\0\0\0", 5) == 5);
