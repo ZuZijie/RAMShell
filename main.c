9bc0c25bc96f201f7629b1b486c24aa0b2e81d14
ZuZijie <14937125+zoinve@user.noreply.gitee.com>
2024-12-30 09:41:18 +0800

    compile

--- main.c
+++ main.c
@@ -3,18 +3,17 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
-#include "stdio.h"
 
 int notin(int fd, int *fds, int n) {
     for (int i = 0; i < n; i++) {
-        if (fds[i] == fd) return 0;
+    if (fds[i] == fd) return 0;
     }
     return 1;
 }
 int genfd(int *fds, int n) {
     for (int i = 0; i < 4096; i++) {
-        if (notin(i, fds, n))
-            return i;
+    if (notin(i, fds, n))
+    return i;
     }
     return -1;
 }
@@ -33,9 +32,41 @@ int main() {
     assert(rread(fd[2], buf, 1) == -1);
     assert(rread(fd[3], buf, 1) == -1);
     for (int i = 0; i < 100; i++) {
-    assert(rwrite(fd[0], "\0\0\0\0\0", 10) == 10);
-    assert(rwrite(fd[1], "hello", 5) == 5);
-    assert(rwrite(fd[2], "world", 5) == 5);
-    assert(rwrite(fd[3], "\x001\x002\x003\x0fe\x0ff", 5) == 5);
+      assert(rwrite(fd[0], "\0\0\0\0\0", 5) == 5);
+      assert(rwrite(fd[1], "hello", 5) == 5);
+      assert(rwrite(fd[2], "world", 5) == 5);
+      assert(rwrite(fd[3], "\x001\x002\x003\x0fe\x0ff", 5) == 5);
     }
+    assert(rclose(fd[0]) == 0);
+    // assert(rclose(fd[1]) == 0);
+    // assert(rclose(fd[2]) == 0);
+    // assert(rclose(fd[3]) == 0);
+    // assert(rclose(genfd(fd, 4)) == -1);
+    // assert((fd[0] = ropen("/0", O_CREAT | O_RDONLY)) >= 0);
+    // assert((fd[1] = ropen("/1", O_CREAT | O_RDONLY)) >= 0);
+    // assert((fd[2] = ropen("/2", O_CREAT | O_RDONLY)) >= 0);
+    // assert((fd[3] = ropen("/3", O_CREAT | O_RDONLY)) >= 0);
+    // assert(rwrite(fd[0], buf, 1) == -1);
+    // assert(rwrite(fd[1], buf, 1) == -1);
+    // assert(rwrite(fd[2], buf, 1) == -1);
+    // assert(rwrite(fd[3], buf, 1) == -1);
+    // for (int i = 0; i < 50; i++) {
+    //   assert(rread(fd[0], buf, 10) == 10);
+    //   assert(memcmp(buf, "\0\0\0\0\0\0\0\0\0\0", 10) == 0);
+    //   assert(rread(fd[1], buf, 10) == 10);
+    //   assert(memcmp(buf, "hellohello", 10) == 0);
+    //   assert(rread(fd[2], buf, 10) == 10);
+    //   assert(memcmp(buf, "worldworld", 10) == 0);
+    //   assert(rread(fd[3], buf, 10) == 10);
+    //   assert(memcmp(buf, "\x001\x002\x003\x0fe\x0ff\x001\x002\x003\x0fe\x0ff", 10) == 0);
+    // }
+    // assert(rread(fd[0], buf, 10) == 0);
+    // assert(rread(fd[1], buf, 10) == 0);
+    // assert(rread(fd[2], buf, 10) == 0);
+    // assert(rread(fd[3], buf, 10) == 0);
+    // assert(rclose(fd[0]) == 0);
+    // assert(rclose(fd[1]) == 0);
+    // assert(rclose(fd[2]) == 0);
+    // assert(rclose(fd[3]) == 0);
+    // return 0;
 }
